101d6762cb56973ffb93b77b5ba05bfb5612e362
Gigi6027 <97170431+Gigi6027@users.noreply.github.com>
2022-09-16 08:46:11 +0530

    Update Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,1 +1,13 @@
-
+!pip install pandas
+import pandas as pd
+myDf=pd.read_csv('B.csv')
+myDf.head()
+myOrd1=myDf['Day']
+myOrd2=myDf['Coin']
+myCrossTable=pd.crosstab(myOrd1,myOrd2)
+myCrossTable
+up=myCrossTable
+up
+up.style.background_gradient(cmap='YlOrRd')
+import seaborn as sns
+sns.heatmap(up,annot=True,cmap='YlOrRd')
